261cd04096325c24b726fef923c7217613017160
Matthew <Bean6754@users.noreply.github.com>
2017-03-09 15:32:02 +0000

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -18,8 +18,8 @@ var x = setInterval(function() {
 
   // Display the result in the element with id="demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s " . "to March 13th.";
-  // document.getElementById("demo").innerHTML = "to March 13th.";
+  + minutes + "m " + seconds + "s ";
+  document.getElementById("demo").innerHTML = "to March 13th.";
 
   // If the count down is finished, write some text 
   if (distance < 0) {
