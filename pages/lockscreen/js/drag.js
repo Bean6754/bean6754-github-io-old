5b6146d446d7a02d414eff1f55b74020baf0c6a2
Matthew <Bean6754@users.noreply.github.com>
2017-09-08 14:15:44 +0100

    Update drag.js

--- pages/lockscreen/js/drag.js
+++ pages/lockscreen/js/drag.js
@@ -18,7 +18,6 @@
 				// assign default values for top and left properties
 				// if(!targ.style.left) { targ.style.left='0px'};
 				if (!targ.style.top) { targ.style.top='0px'};
-	  			if (!targ.style.top2) { targ.style.top2='100%'};
 
 				// calculate integer values for top and left 
 				// properties
@@ -43,17 +42,7 @@
 			function stopDrag() {
 				drag=false;
 			}
-			function doDrag(e) {
-				if (!drag) {return};
-				if (!e) { var e= window.event};
-				// var targ=e.target?e.target:e.srcElement;
-				// move div element
-				// targ.style.left=coordX+e.clientX-offsetX+'px';
-				targ.style.top2=coordY+e.clientY-offsetY+'%';
-				return false;
-			}
 			window.onload = function() {
 				document.onmousedown = startDrag;
 				document.onmouseup = stopDrag;
-				document.keydown = doDrag;
 			}
