8f8e6bebc68510f466dfee3b348a3fbb8016d7ca
Matthew <Bean6754@users.noreply.github.com>
2017-09-22 11:48:13 +0100

    Update gdpr.js

--- js/gdpr.js
+++ js/gdpr.js
@@ -21,6 +21,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "Article 50 should now be successfully triggered!";
+    document.getElementById("demo").innerHTML = "GDPR should now be successfully implemented!";
   }
 }, 1000);
