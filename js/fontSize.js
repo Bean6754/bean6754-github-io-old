79669c05bc18a0b1df7af9e9eaa141b60cd5f01b
Matthew <Bean6754@users.noreply.github.com>
2017-09-26 13:40:46 +0100

    Update fontSize.js

--- js/fontSize.js
+++ js/fontSize.js
@@ -1,6 +1,3 @@
 function fontFunction() {
-  // document.getElementById("container").style.fontSize = "xx-large";
-  var span = document.getElementById("container");
-    console.log(span);
-  span.style.fontSize = "+10%";
+  document.getElementById("container").style.fontSize = "xx-large";
 }
