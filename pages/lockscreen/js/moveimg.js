42367f6ea1d7417db73e34cbb3f41c0e12bc9ef0
Matthew <Bean6754@users.noreply.github.com>
2018-02-02 14:55:30 +0000

    Update moveimg.js

--- pages/lockscreen/js/moveimg.js
+++ pages/lockscreen/js/moveimg.js
@@ -1,9 +1,9 @@
 function myMove() {
   var elem = document.getElementById("background");   
   var pos = 0;
-  var id = setInterval(frame, 5);
+  var id = setInterval(frame, 1);
   function frame() {
-    if (pos == 350) {
+    if (pos == 650) {
       clearInterval(id);
     } else {
       pos++; 
