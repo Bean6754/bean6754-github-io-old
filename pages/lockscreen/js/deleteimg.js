242a2571c6a02b31b7741649b5bd42f78e698348
Matthew <Bean6754@users.noreply.github.com>
2018-02-09 15:16:22 +0000

    Update and rename delete-img.js to deleteimg.js

--- pages/lockscreen/js/deleteimg.js
+++ pages/lockscreen/js/deleteimg.js
@@ -1,4 +1,4 @@
-function delete-img() {
+function deleteimg() {
   var parent = getElementById('background');
   var child = document.getElementById('background-img');
   parent.removeChild(background-img);
