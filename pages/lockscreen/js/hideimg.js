a2708e038fed3e8c2a0c3714b3e584558866f5df
Matthew <Bean6754@users.noreply.github.com>
2018-02-09 18:24:15 +0000

    Update hideimg.js

--- pages/lockscreen/js/hideimg.js
+++ pages/lockscreen/js/hideimg.js
@@ -12,20 +12,8 @@ function hideimg() {
     }
   }
   
-  function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function demo() {
-  console.log('Taking a break...');
-  await sleep(2000);
-  console.log('Two second later');
-}
-
-demo();
-  
-  $(document).ready(function(){
+  /*$(document).ready(function(){
     $("#lockscreen").remove();
-  });
+  });*/
 }
   
