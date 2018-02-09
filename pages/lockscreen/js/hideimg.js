48f3aee6cc6a11d8025eaa58698039cf092e36a8
Matthew <Bean6754@users.noreply.github.com>
2018-02-09 15:44:05 +0000

    Update hideimg.js

--- pages/lockscreen/js/hideimg.js
+++ pages/lockscreen/js/hideimg.js
@@ -1,4 +1,17 @@
 function hideimg() {
+  var elem = document.getElementById("backgroundimg");
+  var pos = 0;
+  var id = setInterval(frame, 20);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos--;
+      elem.style.top = pos + 'px';
+      /* elem.style.left = pos + 'px'; */
+    }
+  }
+  
   var img = document.getElementById('backgroundimg');
     img.style.visibility = 'hidden';
 }
