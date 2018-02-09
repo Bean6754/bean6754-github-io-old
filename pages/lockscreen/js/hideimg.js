81e1db830ca5deff6659c0a8dbe6441e5a2a9f4b
Matthew <Bean6754@users.noreply.github.com>
2018-02-09 18:59:49 +0000

    Update hideimg.js

--- pages/lockscreen/js/hideimg.js
+++ pages/lockscreen/js/hideimg.js
@@ -1,13 +1,13 @@
 function hideimg() {
 $("#lockscreen").click(function(){
-  var dest = parseInt($("#lockscreen").css("margin-bottom").replace("px", "")) + 100;
+  var dest = parseInt($("#lockscreen img").css("margin-bottom").replace("px", "")) + 100;
     if (dest > 500) {
-        $("#lockscreen").animate({
+        $("#lockscreen img").animate({
             marginBottom: "10px"
           }, 500 );
     }
     else {
-      $("#lockscreen").animate({
+      $("#lockscreen img").animate({
         marginBottom: dest + "px"
       }, 500 );
     }
