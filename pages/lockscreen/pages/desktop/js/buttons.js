bcf59bbfd62044acb17a16e3c34616b8dc6b74dc
Matthew <Bean6754@users.noreply.github.com>
2018-04-28 12:57:35 +0100

    Create buttons.js

---
+++ pages/lockscreen/pages/desktop/js/buttons.js
@@ -0,0 +1,22 @@
+var active = 0;
+
+function orbClick() {
+
+if (active == 0) {
+	var div = document.getElementById('orbdiv');
+	div.innerHTML += '<button onClick="">User</button>\
+	<a href="../../index.htm">Sign out</a><br>\
+	<a href="https://github.com/bean6754">About Me</a>\
+	<link href="css/orb.css" rel="stylesheet" type="text/css">\
+	';
+	
+	active = 1;
+	// window.location.href = '../../index.htm';
+	} else {
+		// var div = document.getElementById('orbdiv');
+		// div.innerHTML += '';
+		document.getElementById("orbdiv").innerHTML = '';
+		
+		active = 0;
+	}
+}
