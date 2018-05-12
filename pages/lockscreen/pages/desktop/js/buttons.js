b9cb14b6b5ebf5f6dba9b57db1688c7384c31a98
Matthew <Bean6754@users.noreply.github.com>
2018-05-12 19:38:12 +0100

    Update buttons.js

--- pages/lockscreen/pages/desktop/js/buttons.js
+++ pages/lockscreen/pages/desktop/js/buttons.js
@@ -4,7 +4,7 @@ function orbClick() {
 
 if (active == 0) {
 	var div = document.getElementById('orbdiv');
-	div.innerHTML += '<button onClick="">User</button>\
+	div.innerHTML += '<button onClick="">User</button><br>\
 	<a href="../../index.htm">Sign out</a><br>\
 	<a href="https://github.com/bean6754">About Me</a>\
 	<link href="css/orb.css" rel="stylesheet" type="text/css">\
