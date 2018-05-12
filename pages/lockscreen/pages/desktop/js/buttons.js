16c5b000dea86bf4e9e191d2c64d7932d0f5ddf4
Matthew <Bean6754@users.noreply.github.com>
2018-05-12 19:47:26 +0100

    Update buttons.js

--- pages/lockscreen/pages/desktop/js/buttons.js
+++ pages/lockscreen/pages/desktop/js/buttons.js
@@ -4,11 +4,12 @@ function orbClick() {
 
 if (active == 0) {
 	var div = document.getElementById('orbdiv');
-	div.innerHTML += '<link href="css/orb.css" rel="stylesheet" type="text/css">';
-	var div2 = document.getElementById('orbdiv-contents');
-	div.innerHTML += '<button onClick="">User</button><br>\
-	<a href="../../index.htm">Sign out</a><br>\
-	<a href="https://github.com/bean6754">About Me</a>\
+	div.innerHTML += '<link href="css/orb.css" rel="stylesheet" type="text/css">\
+	<div id="orbdiv-contents">\
+		<button onClick="">User</button><br>\
+		<a href="../../index.htm">Sign out</a><br>\
+		<a href="https://github.com/bean6754">About Me</a>\
+	</div>
 	';
 	
 	active = 1;
@@ -17,7 +18,6 @@ if (active == 0) {
 		// var div = document.getElementById('orbdiv');
 		// div.innerHTML += '';
 		document.getElementById("orbdiv").innerHTML = '';
-		document.getElementById("orbdiv-contents").innerHTML = '';
 		
 		active = 0;
 	}
